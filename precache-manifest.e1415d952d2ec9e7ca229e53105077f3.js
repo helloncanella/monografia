92559a123698e0776780d963c676dcb2692155c9
Hellon Canella <helloncanella@gmail.com>
2019-04-24 10:31:41 -0300

    Updates

--- precache-manifest.e1415d952d2ec9e7ca229e53105077f3.js
+++ precache-manifest.e1415d952d2ec9e7ca229e53105077f3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/monografia/static/js/runtime~main.d8f360ca.js"
   },
   {
-    "revision": "01c1f436f8b4fb9e1013",
-    "url": "/monografia/static/js/main.01c1f436.chunk.js"
+    "revision": "f375f1d26c4daa09e12e",
+    "url": "/monografia/static/js/main.f375f1d2.chunk.js"
   },
   {
     "revision": "4946adf0f18cabaf4db6",
     "url": "/monografia/static/js/2.4946adf0.chunk.js"
   },
   {
-    "revision": "01c1f436f8b4fb9e1013",
+    "revision": "f375f1d26c4daa09e12e",
     "url": "/monografia/static/css/main.d1489539.chunk.css"
   },
   {
-    "revision": "a61efa1e4488faa37afbad1c3238b02c",
+    "revision": "cff98c7ab055a2353770cc63b8adca09",
     "url": "/monografia/index.html"
   }
 ];
