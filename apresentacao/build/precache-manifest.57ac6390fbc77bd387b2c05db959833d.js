1c75fabf380ad85a92b07228c19156254d9ca5fd
Hellon Canella <helloncanella@gmail.com>
2019-04-23 22:06:47 -0300

    added a build

---
+++ apresentacao/build/precache-manifest.57ac6390fbc77bd387b2c05db959833d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ec9e0be216dcce80b6a4",
+    "url": "/static/js/main.ec9e0be2.chunk.js"
+  },
+  {
+    "revision": "4946adf0f18cabaf4db6",
+    "url": "/static/js/2.4946adf0.chunk.js"
+  },
+  {
+    "revision": "ec9e0be216dcce80b6a4",
+    "url": "/static/css/main.d1489539.chunk.css"
+  },
+  {
+    "revision": "b11bd683ac781b26daa2261585b4aff6",
+    "url": "/index.html"
+  }
+];
